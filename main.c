127dc145f5a4cc0756a61096a962fe3cccfbd914
gparamas <gautham.paramasivam2006@gmail.com>
2026-06-10 12:47:37 -0400

    testing malloc

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include "uart.h"
+#include <stdlib.h>
 
 int main() {
+	int* ptr = malloc(100);
 	for(int i = 0; i < 100; i++) {
-		uart_putchar(i);
+		ptr[i] = i;
+	}
+	for(int i = 0; i < 100; i++) {
+		uart_putchar(ptr[i]);
 	}
 }
