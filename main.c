bd6fe20fb0a2ff5d5a6b728c18231dae3775344f
gparamas <gautham.paramasivam2006@gmail.com>
2026-06-11 14:10:04 -0400

    testing overlapping memory

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "uart.h"
-#include <stdlib.h>
+//#include <stdlib.h>
 
 int main() {
-	int* ptr = malloc(100);
+	/*int* ptr = malloc(100);
 	for(int i = 0; i < 100; i++) {
 		ptr[i] = i;
-	}
+	}*/
 	for(int i = 0; i < 100; i++) {
-		uart_putchar(ptr[i]);
+		uart_putchar(i);
 	}
 }
