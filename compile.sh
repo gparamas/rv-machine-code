fc4976c08fa2018c7a118e26be429fb2348b1716
gparamas <gautham.paramasivam2006@gmail.com>
2026-06-28 14:00:57 -0400

    test qspi integration

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-riscv32-unknown-elf-gcc -O3 -T link.ld -nostartfiles start.S uart.c $1.c -o $1
-riscv32-unknown-elf-objcopy -O binary $1 $1.bin  
-printf '\xff\xff\xff\xff' >> main.bin
-riscv32-unknown-elf-objdump -D -b binary -m riscv $1.bin
+riscv32-unknown-elf-gcc -O3 -T link.ld -nostartfiles start.S uart.c qspi.c src/$1.c -o elf/$1
+riscv32-unknown-elf-objcopy -O binary elf/$1 bin/$1.bin  
+printf '\xff\xff\xff\xff' >> bin/$1.bin
+riscv32-unknown-elf-objdump -D -b binary -m riscv bin/$1.bin
 
 
--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
-#include "uart.h"
+#include "../uart.h"
 //#include <stdlib.h>
+#include "../qspi.h"
 
 int main() {
 	/*int* ptr = malloc(100);
 	for(int i = 0; i < 100; i++) {
 		ptr[i] = i;
 	}*/
-	for(int i = 0; i < 100; i++) {
-		uart_putchar(i);
+	uint32_t array[10] = {0};
+
+	send_wren();
+	send_wrsr();
+	send_rdsr(1);
+	send_4read_noXIP(0x00, 10, 0, array);
+	for(int i = 0; i < 10; i++) {
+		uart_putchar(array[i]);
 	}
 }
